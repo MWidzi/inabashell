34b3c189ba1191196da2886a0a2b334f0c4e912a
Maciej Widzinski <mlydzy123@gmail.com>
2026-03-25 13:20:32 +0100

    deletions and slight changes

--- scripts/rofiWifiWrapper.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cp ~/.config/alt_styles/rofi/wifi_menu/theme.rasi ~/.config/rofi/theme.rasi
-bash ~/Documents/scripts/rofi-wifi-menu/rofi-wifi-menu.sh
-cp ~/.config/alt_styles/rofi/normal/theme.rasi ~/.config/rofi/theme.rasi
